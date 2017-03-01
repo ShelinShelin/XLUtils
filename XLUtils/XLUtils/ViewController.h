dafd264e185c664c550cfcfe984afcad5ab3eb70
Shelin <1332109343@qq.com>
2017-03-01 14:39:41 +0800

    first commit

---
+++ XLUtils/XLUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLUtils
+//
+//  Created by Shelin on 17/3/1.
+//  Copyright © 2017年 StarUnion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XLUtils/XLUtils/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XLUtils
+//
+//  Created by Shelin on 17/3/1.
+//  Copyright © 2017年 StarUnion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
